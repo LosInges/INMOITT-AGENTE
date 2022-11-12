ed8d9731ce8cac358274eead618284c6aef8ffe2
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-11-11 19:56:30 -0700

    Fotos Minuatura AGENTE

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+
 import { Agente } from 'src/app/interfaces/agente';
-import { Inmobiliaria } from '../interfaces/inmobiliaria';
 import { AgenteService } from '../services/agente.service';
+import { AlertController } from '@ionic/angular';
+import { FotoService } from './../services/foto.service';
+import { Inmobiliaria } from '../interfaces/inmobiliaria';
 import { InmobiliariaService } from '../services/inmobiliaria.service';
 import { SessionService } from '../services/session.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-perfil',
@@ -12,11 +16,10 @@ import { SessionService } from '../services/session.service';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
-
-  apellidoPat: string = ''
-  apellidoMat: string = ''
-  inmobiliarias: Inmobiliaria[]
-
+  apellidoPat: string = '';
+  apellidoMat: string = '';
+  inmobiliarias: Inmobiliaria[];
+  api = environment.api;
   agente: Agente = {
     rfc: '',
     inmobiliaria: '',
@@ -25,31 +28,44 @@ export class PerfilPage implements OnInit {
     password: '',
     apellido: '',
     telefono: '',
-    foto: ''
+    foto: '',
   };
   confirmPassword = '';
+  mensaje = '';
 
   constructor(
     private sessionService: SessionService,
     private inmobiliariaService: InmobiliariaService,
     private agenteService: AgenteService,
-    private router:  Router
-  ) { }
+    private router: Router,
+    private fotoService: FotoService,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
-    this.sessionService.get('rfc')?.then(rfc => {
-      if(rfc) this.agenteService.getAgente(rfc).subscribe(agente => {
-        this.agente = agente
-        this.apellidoPat = agente.apellido.split(" ")[0]
-        this.apellidoMat = agente.apellido.split(" ")[1]
-      })
-    })
-    this.inmobiliariaService.getInmobiliarias()?.subscribe(inmobiliarias => {
-      this.inmobiliarias = inmobiliarias
-    })
+    this.sessionService.get('inmobiliaria').then((inmobiliaria) => {
+      if (inmobiliaria) {
+        this.sessionService.get('rfc')?.then((rfc) => {
+          if (rfc) {
+            this.agenteService
+              .getAgente(inmobiliaria, rfc)
+              .subscribe((agente) => {
+                this.agente = agente;
+                console.log(agente);
+                this.apellidoPat = agente.apellido.split(' ')[0];
+                this.apellidoMat = agente.apellido.split(' ')[1];
+              });
+          }
+        });
+      }
+    });
+
+    this.inmobiliariaService.getInmobiliarias()?.subscribe((inmobiliarias) => {
+      this.inmobiliarias = inmobiliarias;
+    });
   }
 
-  actualizarPerfil() { 
+  actualizarPerfil() {
     // if (
     //   this.agente.rfc.trim() !== "" &&
     //   this.agente.inmobiliaria.trim() !== "" &&
@@ -60,24 +76,70 @@ export class PerfilPage implements OnInit {
     //   this.agente.foto.trim() !== "" &&
     //   this.agente.telefono.trim() !== "" &&
     //   this.confirmPassword.trim() !== ""
-    // ) 
+    // )
     //{
-      if (this.confirmPassword === this.agente.password)
-      {
-        this.agente.apellido = this.apellidoPat +' '+ this.apellidoMat;  
-        this.agenteService.postAgente(this.agente).subscribe(res => console.log(res))
-      }
+    if (this.confirmPassword === this.agente.password) {
+      this.agente.apellido = this.apellidoPat + ' ' + this.apellidoMat;
+      this.agenteService
+        .postAgente(this.agente)
+        .subscribe((res) => console.log(res));
+        this.mensaje = "Actualización EXITOSA"
+       
+    }else{
+      this.mensaje ="Ingrese Todos los valores"
+    }
+    this.presentAlert(this.mensaje)
     //}
   }
 
-  eliminarPerfil(){
-    if (this.confirmPassword === this.agente.password)
-    this.agenteService.deleteAgente(this.agente.rfc).subscribe(res => {
-      if(res.results)
-      this.sessionService.clear().then(()=>this.router.navigate([""]))
-      else console.log(res)
-    })
+  async presentAlert(mensaje) {
+    const alert = await this.alertController.create({
+      header: this.mensaje,
+      cssClass: 'custom-alert',
+      buttons: [
+        {
+          text: 'NO',
+          cssClass: 'alert-button-cancel',
+        },
+        {
+          text: 'OK',
+          cssClass: 'alert-button-confirm',
+        },
+      ],
+    });
 
+      await alert.present();
+    }
+
+  eliminarPerfil() {
+    if (this.confirmPassword === this.agente.password)
+      this.agenteService.deleteAgente(this.agente.rfc).subscribe((res) => {
+        if (res.results)
+          this.sessionService.clear().then(() => this.router.navigate(['']));
+        else console.log(res);
+      });
   }
 
+  tomarFotografia() {
+    this.fotoService.tomarFoto().then((photo) => {
+      // this.fotoService.subirMiniatura(photo.webPath).subscribe((data) => {
+      //   console.log(data);
+      // });
+      console.log(photo);
+      const reader = new FileReader();
+      const datos = new FormData();
+      reader.onload = () => {
+        const imgBlob = new Blob([reader.result], {
+          type: `image/${photo.format}`,
+        });
+        datos.append('img', imgBlob, `imagen.${photo.format}`);
+        this.fotoService
+          .subirMiniatura(datos)
+          .subscribe((res) => (this.agente.foto = res.path));
+      };
+      const consulta = fetch(photo.webPath).then((v) =>
+        v.blob().then((imagen) => reader.readAsArrayBuffer(imagen))
+      );
+    });
+  }
 }
